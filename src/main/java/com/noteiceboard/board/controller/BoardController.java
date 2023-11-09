37f988f8a8d410f2f2eaefca1a4b572d7e6feb0f
Tanguri <jungwontwo2@naver.com>
2023-11-09 14:55:10 +0900

    5 게시글 조회

--- src/main/java/com/noteiceboard/board/controller/BoardController.java
+++ src/main/java/com/noteiceboard/board/controller/BoardController.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,21 +17,30 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/save")
-    public String saveForm(){
+    public String saveForm() {
         return "save";
     }
+
     @PostMapping("/save")
-    public String save(@ModelAttribute BoardDTO boardDTO){
+    public String save(@ModelAttribute BoardDTO boardDTO) {
         System.out.println("boardDTO = " + boardDTO);
         boardService.save(boardDTO);
         return "index";
     }
 
     @GetMapping("/")
-    public String findAll(Model model){
+    public String findAll(Model model) {
         //db에서 전체 개시글 데이터를 가져와서 list.html에 보여준다.
         List<BoardDTO> boardDTOList = boardService.findAll();
         model.addAttribute("boardList", boardDTOList);
         return "list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model){
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board",boardDTO);
+        return "detail";
     }
 }
